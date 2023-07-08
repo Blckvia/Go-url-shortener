eab6a9e427fcc4cce8e6deb702630aa0c4048e3c
blckvia <a.vyatkinn@icloud.com>
2023-07-08 21:22:39 +0300

    feat: logging has been set

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,11 +2,43 @@ package main
 
 import (
 	"Projects/go-url-shortener/cmd/internal/config"
-	"fmt"
+	"os"
+
+	"golang.org/x/exp/slog"
+)
+
+const (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
 }
