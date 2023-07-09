96c38cf74c0fd0c7d2a0796439adc3bd8934044a
blckvia <a.vyatkinn@icloud.com>
2023-07-09 12:08:40 +0300

    feat: added handler for getting more beautiful logs while env = local

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -5,6 +5,7 @@ import (
 	mwLogger "Projects/go-url-shortener/cmd/internal/http-server/middleware/logger"
 	"Projects/go-url-shortener/cmd/internal/lib/logger/sl"
 	"Projects/go-url-shortener/cmd/internal/storage/sqlite"
+	"Projects/go-url-shortener/cmd/internal/lib/logger/handlers/slogpretty"
 	"os"
 
 	"github.com/go-chi/chi/v5"
@@ -25,6 +26,7 @@ func main() {
 
 	log.Info("starting url-shortener", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
+	log.Error("WTF WTF")
 
 	storage, err := sqlite.New(cfg.StoragePath)
 	if err != nil {
@@ -39,6 +41,8 @@ func main() {
 	router.Use(middleware.RequestID)
 	router.Use(middleware.Logger)
 	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
 }
 
 func setupLogger(env string) *slog.Logger {
@@ -46,9 +50,7 @@ func setupLogger(env string) *slog.Logger {
 
 	switch env {
 	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
+		log = setupPrettySlog()
 	case envDev:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
@@ -60,4 +62,16 @@ func setupLogger(env string) *slog.Logger {
 	}
 
 	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
